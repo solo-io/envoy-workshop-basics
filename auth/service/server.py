07d76f2e1bb43f74219b3d9e191940bd4a426d23
asayah <adam.sayah@solo.io>
2022-03-18 17:00:17 -0700

    wip

--- auth/service/server.py
+++ auth/service/server.py
@@ -1,12 +1,16 @@
-from flask import Flask, request
+from flask import Flask, request, abort
+
 
 app = Flask(__name__)
 
-
-@app.route('/auth')
-def hello():
-    return 'Hello ' + request.headers.get('x-current-user') + ' from behind Envoy!'
-
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def auth(path):
+    if 'authorization' in request.headers:
+        if request.headers['authorization'] == 'workshop':
+                return 'Allowed!'
+        
+    abort(403)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=False)
--- upstream/service/server.py
+++ upstream/service/server.py
@@ -3,9 +3,11 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
-@app.route('/service')
-def hello():
-    return 'Hello ' + request.headers.get('x-current-user') + ' from behind Envoy!'
+@app.route('/', defaults={'u_path': ''})
+@app.route('/<path:u_path>')
+def catch_all(u_path):
+    print(repr(u_path))
+    return 'Hello back!'
 
 
 if __name__ == "__main__":
